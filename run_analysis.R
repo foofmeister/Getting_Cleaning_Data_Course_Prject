b71846234026eadc54a9630ca7fa3bd31f8b7590
foofmeister <forest.summers@gmail.com>
2016-05-05 15:35:12 -0600

    Added files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -1,40 +1,45 @@
 
-#Download package and set working directory
+### FOREST R HOMEWORK
 
+
+## Install Packages
 library(RCurl)
 library(plyr)
 library(dplyr)
+
+
+## Working Directory
 setwd("C:/Users/summers.forest/R/Coursera_Data_Cleaning")
 
-##Location of Zip
 
+
+## Download Zip
 urlzip <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(urlzip,"SMARTPHONE.zip")
 unzip("SMARTPHONE.zip")
 
-##read Tables
 
+## Train & Test Data Sets
 x_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
 x_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
-
 y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")
 y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
 
-sub_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
-sub_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
 
+## Merge Train/Test Data Sets
+data <- rbind(x_train, x_test)
 yvar <- rbind(y_train, y_test)
-
 yvar <- plyr::rename(yvar,c("V1"="Activity"))
 
-sub <- rbind(sub_train, sub_test)
 
+## Subject/Participant Identifiers
+sub_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+sub_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+sub <- rbind(sub_train, sub_test)
 sub <- plyr::rename(sub,c("V1"="Subject"))
 
-data <- rbind(x_train, x_test)
 
-#Download Features
-
+## Features
 features <- read.table("./UCI HAR Dataset/features.txt")
 
 #Change Names of Data Set to Actual Names using 
@@ -61,5 +66,11 @@ data3 <- merge(activity_labels,data2,by.x="V1",by.y="Activity")
 data3 <- plyr::rename(data3,c("V2"="Activity"))
 
 Final_Data <- select(data3,-V1)
+#Final_Data <- data3[c(-1)]
 
-mean_std <- features[grepl("june", tolower(m$Special.Notes))]
+
+## Mean Aggregations
+Data_Agg <- aggregate(. ~ Activity + Subject, data = Final_Data, FUN=function(Final_Data) c(mn=mean(Final_Data), n=length(Final_Data) ) )
+
+
+
